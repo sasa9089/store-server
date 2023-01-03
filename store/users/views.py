e220d920d729bf55b0f3358d7e28f7e3ecaf59fe
Александр Петров <sasa9089@yandex.ru>
2023-01-03 18:46:20 +0300

    страница авторизации, функции под нее и работа с шаблоном

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponsePermanentRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.models import User
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponsePermanentRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    contex = {'form': form}
+    return render(request, 'users/login.html', contex)
 
 
 def registration(request):
