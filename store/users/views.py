aa9ebb61d61cade3188dacbd247032daab95f7f3
Александр Петров <sasa9089@yandex.ru>
2023-01-05 18:06:03 +0300

    авторизация, регистрация, проверки пользователей и т п

--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, HttpResponsePermanentRedirect
-from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -15,7 +14,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponsePermanentRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse('index'))
     else:
         form = UserLoginForm()
     contex = {'form': form}
@@ -23,4 +22,34 @@ def login(request):
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Поздравляем! Вы успешно зарегистрированы!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    contex = {'form': form}
+    return render(request, 'users/registration.html', contex)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user,
+                               data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.add_errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    contex = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', contex)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
